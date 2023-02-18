7d864516ed0acab29ed22a0554264ea170efbbb2
Michele Ceccacci <michele.ceccacci@studio.unibo.it>
2023-02-18 20:56:05 +0100

    generate project

---
+++ app/src/main/java/homer/App.java
@@ -0,0 +1,10 @@
+package homer;
+
+public final class App {
+    public static void main(String[] args) {
+    }
+
+    public static String getGreeting() {
+        return "Hello, World!";
+    }
+}
