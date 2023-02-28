cf40494bd2b84740c5a40f52ee233f1e90abb09c
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-02-28 16:45:28 +0100

    prevent App object instantiation

--- app/src/main/java/homer/App.java
+++ app/src/main/java/homer/App.java
@@ -1,6 +1,10 @@
 package homer;
 
 public final class App {
+    private App() {
+        throw new UnsupportedOperationException("App should not be instantiated");
+    }
+
     public static void main(final String[] args) {
     }
 
