c826f15a701fe61e97ee6d45164a9a405d13b494
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-02-28 16:43:00 +0100

    use final where possible

--- app/src/main/java/homer/App.java
+++ app/src/main/java/homer/App.java
@@ -1,7 +1,7 @@
 package homer;
 
 public final class App {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
     }
 
     public static String getGreeting() {
