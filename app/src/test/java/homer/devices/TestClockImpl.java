1d760bf6961678bb8d1266cd2c9e836b9c91469b
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-03-01 20:59:14 +0100

    [clock] create test for ClockImpl

---
+++ app/src/test/java/homer/devices/TestClockImpl.java
@@ -0,0 +1,50 @@
+package homer.devices;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAmount;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import homer.api.Clock;
+
+final class TestClockImpl {
+
+    private Clock clock;
+
+    @BeforeEach
+    void createClock() {
+        this.clock = new ClockImpl();
+        assertNotNull(this.clock);
+    }
+
+    @Test
+    void testGetTime() {
+        assertNotNull(this.clock.getDateTime());
+    }
+
+    @Test
+    void testSetTime() {
+        final LocalDateTime newDateTime = LocalDateTime.now();
+        this.clock.setDateTime(newDateTime);
+        assertEquals(this.clock.getDateTime(), newDateTime);
+    }
+
+    @Test
+    void testUpdateTick() {
+        checkTick(Duration.ofSeconds(1));
+        checkTick(Duration.ofHours(1));
+        checkTick(Duration.ofDays(1));
+    }
+
+    private void checkTick(final TemporalAmount deltaTime) {
+        final LocalDateTime currentDateTime = this.clock.getDateTime();
+        this.clock.updateTick(deltaTime);
+        assertEquals(this.clock.getDateTime(), currentDateTime.plus(deltaTime));
+    }
+
+}
