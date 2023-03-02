abe8ab90c2426ee50cd51fbc323b3957cb543b86
Michele Ceccacci <michele.ceccacci@studio.unibo.it>
2023-03-02 10:06:15 +0100

    [common] add DeviceInfoImpl

---
+++ app/src/main/java/homer/DeviceInfoImpl.java
@@ -0,0 +1,28 @@
+package homer;
+
+import homer.api.DeviceInfo;
+
+public final class DeviceInfoImpl implements DeviceInfo {
+    private final long id;
+    private final String type;
+
+    /**
+     * @param id considered final.
+     * @param type considered final. 
+     */
+    public DeviceInfoImpl(final long id, final String type) {
+        this.id = id;
+        this.type = type;
+    }
+
+    @Override
+    public long getID() {
+        return this.id;
+    }
+
+    @Override
+    public String getType() {
+        return this.type;
+    }
+
+}
