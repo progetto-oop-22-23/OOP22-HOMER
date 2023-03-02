fb9f0240a83c8c0a654a70448b1a98522e160c92
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-03-02 11:17:56 +0100

    [FIX] removed redundant cutPowerTo impl

---
+++ app/src/main/java/homer/impl/ElectricalMeterImpl.java
@@ -0,0 +1,44 @@
+package homer.impl;
+
+import homer.api.Device;
+import homer.api.ElectricalMeter;
+
+public class ElectricalMeterImpl implements ElectricalMeter<Device<?>> {
+
+    @Override
+    public void checkConsumption() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'checkConsumption'");
+    }
+
+    @Override
+    public double getGlobalConsumption() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getGlobalConsumption'");
+    }
+
+    @Override
+    public void setGlobalConsumption(double globalConsumption) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setGlobalConsumption'");
+    }
+
+    @Override
+    public double getMaxConsumption() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMaxConsumption'");
+    }
+
+    @Override
+    public void setMaxConsumption(double maxConsumption) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setMaxConsumption'");
+    }
+
+    @Override
+    public void cutPowerTo(Device<?> Device) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cutPowerTo'");
+    }
+
+}
