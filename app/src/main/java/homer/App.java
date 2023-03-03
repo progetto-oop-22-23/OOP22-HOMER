bf7e0bdf7e6c898b60dae5fc977ec2a264542fa9
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-03-03 16:37:32 +0100

    add missing javadoc comments

--- app/src/main/java/homer/App.java
+++ app/src/main/java/homer/App.java
@@ -1,10 +1,18 @@
 package homer;
 
+/**
+ * Main application class.
+ */
 public final class App {
     private App() {
         throw new UnsupportedOperationException("App should not be instantiated");
     }
 
+    /**
+     * Application entry point.
+     * 
+     * @param args Command line arguments
+     */
     public static void main(final String[] args) {
     }
 
