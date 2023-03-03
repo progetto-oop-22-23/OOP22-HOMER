2a6e43db0b46714aa9e5a7e93fb5fd6ac74138ae
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-03-03 17:59:29 +0100

    [OUTLETS] refactor
    Fixed some linter errors and removed magic numbers

--- app/src/main/java/homer/impl/Outlets/LOutlet.java
+++ app/src/main/java/homer/impl/Outlets/LOutlet.java
@@ -4,22 +4,25 @@ import homer.api.Device;
 import homer.api.DeviceInfo;
 
 /**
- * Models a standard L-type outlet, which supports a maximum power drain of 3.5kW.
+ * Models a standard L-type outlet, which supports a maximum power drain of
+ * 3.5kW.
  * See {@link homer.impl.Outlets.Outlet}
  */
 
 public final class LOutlet extends Outlet {
+
+    private static final double MAX_VALUE = 3.5;
+    private static final double MIN_VALUE = 0;
+
     /**
      * LOutlet constructor.
      * 
      * @param info
      * @param state
-     * @param minValue 0kW
-     * @param maxValue 3.5kW
      * @param device
      */
-    public LOutlet(DeviceInfo info, double state, Device<?> device) {
-        super(info, state, 0, 3.5, device);
+    public LOutlet(final DeviceInfo info, final double state, final Device<?> device) {
+        super(info, state, MIN_VALUE, MAX_VALUE, device);
     }
 
 }
