206eafd5a637edc3b5ab749b32d57f128fce9db3
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-03-03 17:41:41 +0100

    [OUTLETS] Implement L-type outlet

---
+++ app/src/main/java/homer/impl/Outlets/LOutlet.java
@@ -0,0 +1,25 @@
+package homer.impl.Outlets;
+
+import homer.api.Device;
+import homer.api.DeviceInfo;
+
+/**
+ * Models a standard L-type outlet, which supports a maximum power drain of 2kW.
+ * See {@link homer.impl.Outlets.Outlet}
+ */
+
+public final class LOutlet extends Outlet {
+    /**
+     * LOutlet constructor.
+     * 
+     * @param info
+     * @param state
+     * @param minValue 0kW
+     * @param maxValue 3.5kW
+     * @param device
+     */
+    public LOutlet(DeviceInfo info, double state, Device<?> device) {
+        super(info, state, 0, 3.5, device);
+    }
+
+}
