4a6660d33c6a94a4d30b44731175b1e5cae9b0b0
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-03-06 18:04:06 +0100

    [lock] add simple lock implementation

---
+++ app/src/main/java/homer/model/lock/SimpleLock.java
@@ -0,0 +1,36 @@
+package homer.model.lock;
+
+import homer.DeviceInfoImpl;
+import homer.api.DeviceIdImpl;
+import homer.api.DeviceInfo;
+import homer.api.ToggleableDevice;
+
+/**
+ * Implementation of a {@link ToggleableDevice} representing a lock.
+ */
+public final class SimpleLock implements ToggleableDevice<Boolean> {
+
+    public static final String DEVICE_TYPE = "SimpleLock";
+    private boolean isLocked = false;
+
+    @Override
+    public DeviceInfo getInfo() {
+        return new DeviceInfoImpl(new DeviceIdImpl(), DEVICE_TYPE);
+    }
+
+    @Override
+    public Boolean getState() {
+        return this.isLocked;
+    }
+
+    @Override
+    public boolean isToggled() {
+        return getState();
+    }
+
+    @Override
+    public void toggle() {
+        this.isLocked = this.isLocked ? false : true;
+    }
+
+}
