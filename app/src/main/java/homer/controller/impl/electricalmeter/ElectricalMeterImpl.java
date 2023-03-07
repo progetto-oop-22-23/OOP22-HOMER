e58caffc866aa7f14e61b28151ead3ba76033851
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-03-07 17:13:00 +0100

    [METER] Create empty implementation of ElectricalMeter

---
+++ app/src/main/java/homer/controller/impl/electricalmeter/ElectricalMeterImpl.java
@@ -0,0 +1,55 @@
+package homer.controller.impl.electricalmeter;
+
+import java.util.ArrayList;
+
+import homer.controller.api.electricalmeter.ElectricalMeter;
+import homer.model.outlets.Outlet;
+
+/**
+ * Implements {@link homer.controller.api.electricalmeter.ElectricalMeter}
+ */
+public final class ElectricalMeterImpl implements ElectricalMeter {
+
+    @Override
+    public ArrayList<Outlet> getOutlets() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getOutlets'");
+    }
+
+    @Override
+    public void addOutlet(Outlet outlet) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addOutlet'");
+    }
+
+    @Override
+    public void removeOutlet(Outlet outlet) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeOutlet'");
+    }
+
+    @Override
+    public void checkConsumption() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'checkConsumption'");
+    }
+
+    @Override
+    public void sortForConsumption() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sortForConsumption'");
+    }
+
+    @Override
+    public void cutPowerTo(Outlet outlet) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'cutPowerTo'");
+    }
+
+    @Override
+    public void restorePowerTo(Outlet outlet) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'restorePowerTo'");
+    }
+    
+}
