4ba09d931e8793dbedcb2bc82e9cddaee0d28a01
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-03-07 17:42:08 +0100

    [METER] Refactor
    WIP

--- app/src/main/java/homer/controller/api/electricalmeter/ElectricalMeter.java
+++ app/src/main/java/homer/controller/api/electricalmeter/ElectricalMeter.java
@@ -1,6 +1,6 @@
 package homer.controller.api.electricalmeter;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import homer.model.outlets.Outlet;
 
@@ -8,6 +8,8 @@ import homer.model.outlets.Outlet;
  * An interface that models an electrical meter.
  * The meter can check the global consumption in the house and cut power
  * to the outlets if their consumption is too high.
+ * 
+ * @author Alessandro Monticelli
  */
 public interface ElectricalMeter {
 
@@ -16,7 +18,13 @@ public interface ElectricalMeter {
      * 
      * @return And ArrayList of Outlet
      */
-    ArrayList<Outlet> getOutlets();
+    List<Outlet> getOutlets();
+
+    /**
+     * Sets the outlets to the new list of outlets.
+     * @param outlets The new list of outlets.
+     */
+    void setOutlets(List<Outlet> outlets);
 
     /**
      * Adds an outlet to the list of outlets controlled by the meter.
@@ -33,9 +41,9 @@ public interface ElectricalMeter {
     void removeOutlet(Outlet outlet);
 
     /**
-     * Checks the value of the global consumption.
+     * Computes and sets the value of the global consumption.
      */
-    void checkConsumption();
+    void computeConsumption();
 
     /**
      * Sorts the list of outlets from the highest-consuming to the least-consuming.
@@ -44,14 +52,14 @@ public interface ElectricalMeter {
 
     /**
      * Cuts power to a {@link homer.model.outlets.Outlet}.
+     * 
      * @param outlet The Outlet which has to be shut.
      */
     void cutPowerTo(Outlet outlet);
 
     /**
-     * Restore the power supply to a {@link homer.model.outlets.Outlet}.
-     * @param outlet The outlet which has to be restored.
+     * 
      */
-    void restorePowerTo(Outlet outlet);
+    void checkConsumption();
 
 }
