83495059558588a49479c85703faf4495583173d
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-03-09 18:30:23 +0100

    [discrete] change javadoc of updateTick

--- app/src/main/java/homer/core/DiscreteObject.java
+++ app/src/main/java/homer/core/DiscreteObject.java
@@ -10,7 +10,7 @@ public interface DiscreteObject {
     /**
      * Sends the time update event to the object.
      * 
-     * @param dt The time difference from the last tick.
+     * @param dt The time that has passed since the last tick.
      */
     void updateTick(TemporalAmount dt);
 
