ef0b62da5f2833749b4b5cf5f24951b4f7cc8e3d
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-03-11 22:02:15 +0100

    [limit] overload limit method for double

--- src/main/java/homer/common/limit/Limit.java
+++ src/main/java/homer/common/limit/Limit.java
@@ -21,4 +21,16 @@ public final class Limit {
         return Math.max(lowerLimit, Math.min(input, upperLimit));
     }
 
+    /**
+     * Limits the input between lower and upper limits.
+     * 
+     * @param input      The input to limit.
+     * @param lowerLimit The lower limit.
+     * @param upperLimit The upper limit.
+     * @return The input limited between the lower and upper limits.
+     */
+    public static double limit(final double input, final double lowerLimit, final double upperLimit) {
+        return Math.max(lowerLimit, Math.min(input, upperLimit));
+    }
+
 }
