3ea3104b019434f782ee2c559784b71da8466221
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-03-11 22:07:54 +0100

    [limit] add test for limit with double values

--- src/test/java/homer/common/limit/TestLimit.java
+++ src/test/java/homer/common/limit/TestLimit.java
@@ -10,12 +10,20 @@ import org.junit.jupiter.api.Test;
 final class TestLimit {
 
     @Test
-    void testLimit() {
+    void testLimitInt() {
         final var lowerLimit = 10;
         final var upperLimit = 30;
         assertEquals(lowerLimit, Limit.limit(0, lowerLimit, upperLimit));
         assertEquals(20, Limit.limit(20, lowerLimit, upperLimit));
-        assertEquals(upperLimit, Limit.limit(40, lowerLimit, upperLimit));
+    }
+
+    @Test
+    void testLimitDouble() {
+        final double lowerLimit = 10.5d;
+        final double upperLimit = 30.5d;
+        assertEquals(lowerLimit, Limit.limit(lowerLimit - 10.0d, lowerLimit, upperLimit));
+        assertEquals(24.5d, Limit.limit(24.5d, lowerLimit, upperLimit));
+        assertEquals(upperLimit, Limit.limit(upperLimit + 10.0d, lowerLimit, upperLimit));
     }
 
 }
