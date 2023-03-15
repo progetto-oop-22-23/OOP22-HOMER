2d07c499efa259e91bbbb616ac4143def26e2285
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-03-15 23:25:36 +0100

    [limit] add check for the correct order of bounds

--- src/main/java/homer/common/limit/Limit.java
+++ src/main/java/homer/common/limit/Limit.java
@@ -18,6 +18,7 @@ public final class Limit {
      * @return The input limited between the lower and upper limits.
      */
     public static int limit(final int input, final int lowerLimit, final int upperLimit) {
+        checkLimitsOrder(lowerLimit, upperLimit);
         return Math.max(lowerLimit, Math.min(input, upperLimit));
     }
 
@@ -30,6 +31,7 @@ public final class Limit {
      * @return The input limited between the lower and upper limits.
      */
     public static double limit(final double input, final double lowerLimit, final double upperLimit) {
+        checkLimitsOrder(lowerLimit, upperLimit);
         return Math.max(lowerLimit, Math.min(input, upperLimit));
     }
 
@@ -43,12 +45,19 @@ public final class Limit {
      * @return The input limited between the lower and upper limits.
      */
     public static <S extends Number & Comparable<S>> S limit(final S input, final S lowerLimit, final S upperLimit) {
+        checkLimitsOrder(lowerLimit, upperLimit);
         if (input.compareTo(lowerLimit) < 0) {
             return lowerLimit;
         } else if (input.compareTo(upperLimit) > 0) {
             return upperLimit;
         } else {
             return input;
+        }
+    }
+
+    private static <S extends Number & Comparable<S>> void checkLimitsOrder(final S lowerLimit, final S upperLimit) {
+        if (lowerLimit.compareTo(upperLimit) > 0) {
+            throw new IllegalArgumentException("lowerLimit and upperLimit are inverted");
         }
     }
 
