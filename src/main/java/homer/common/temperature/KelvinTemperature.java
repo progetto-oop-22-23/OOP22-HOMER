a18c15f51b4eea18fda5112d7b1034bd312ab120
Michele Ceccacci <michele.ceccacci@studio.unibo.it>
2023-03-15 16:45:58 +0100

    [model] implement equals and hashCode in KelvinTemperature

--- src/main/java/homer/common/temperature/KelvinTemperature.java
+++ src/main/java/homer/common/temperature/KelvinTemperature.java
@@ -35,4 +35,29 @@ public final class KelvinTemperature implements Temperature {
         return Double.compare(this.getKelvin(), arg0.getKelvin());
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        long temp;
+        temp = Double.doubleToLongBits(temperature);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        } 
+        if (obj == null) {
+            return false;
+        }
+        if (obj instanceof Temperature) {
+            final Temperature temperature = (Temperature) obj;
+            return this.getKelvin() == temperature.getKelvin();
+        }
+        return false;
+    }
+
 }
