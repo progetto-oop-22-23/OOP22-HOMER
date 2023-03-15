926f038546907169448f85d6dd1c8519b0e9888f
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-03-16 00:09:39 +0100

    [limit] refactor limit tests

--- src/test/java/homer/common/limit/TestLimit.java
+++ src/test/java/homer/common/limit/TestLimit.java
@@ -4,27 +4,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-// CHECKSTYLE: MagicNumber OFF
-// Rule disabled in test class.
-
 final class TestLimit {
 
     @Test
     void testLimitInt() {
         final int lowerLimit = 10;
         final int upperLimit = 30;
-        assertEquals(lowerLimit, Limit.limit(lowerLimit - 10, lowerLimit, upperLimit));
-        assertEquals(20, Limit.limit(20, lowerLimit, upperLimit));
-        assertEquals(upperLimit, Limit.limit(upperLimit + 10, lowerLimit, upperLimit));
+        final int belowRange = lowerLimit - 10;
+        final int inRange = 20;
+        final int aboveRange = upperLimit + 10;
+        assertEquals(lowerLimit, Limit.limit(belowRange, lowerLimit, upperLimit));
+        assertEquals(inRange, Limit.limit(inRange, lowerLimit, upperLimit));
+        assertEquals(upperLimit, Limit.limit(aboveRange, lowerLimit, upperLimit));
     }
 
     @Test
     void testLimitDouble() {
         final double lowerLimit = 10.5d;
         final double upperLimit = 30.5d;
-        assertEquals(lowerLimit, Limit.limit(lowerLimit - 10.0d, lowerLimit, upperLimit));
-        assertEquals(24.5d, Limit.limit(24.5d, lowerLimit, upperLimit));
-        assertEquals(upperLimit, Limit.limit(upperLimit + 10.0d, lowerLimit, upperLimit));
+        final double belowRange = lowerLimit - 10.0d;
+        final double inRange = 24.5d;
+        final double aboveRange = upperLimit + 10.0d;
+        assertEquals(lowerLimit, Limit.limit(belowRange, lowerLimit, upperLimit));
+        assertEquals(inRange, Limit.limit(inRange, lowerLimit, upperLimit));
+        assertEquals(upperLimit, Limit.limit(aboveRange, lowerLimit, upperLimit));
     }
 
 }
