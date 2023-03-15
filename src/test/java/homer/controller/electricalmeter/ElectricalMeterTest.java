3704d1aa7ddb32e8b3ea7fa412f65a2680fbc629
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-03-15 12:01:51 +0100

    [REFACTOR] Use local costants

--- src/test/java/homer/controller/electricalmeter/ElectricalMeterTest.java
+++ src/test/java/homer/controller/electricalmeter/ElectricalMeterTest.java
@@ -19,14 +19,6 @@ final class ElectricalMeterTest {
     private static final String LOUTLET = "LOUTLET";
     private static final String COUTLET = "COUTLET";
     private static final double STATE = 0;
-    private static final int OUTLET_INDEX_TO_REMOVE = 3;
-    private static final int EXPECTED_OUTLET_LIST_SIZE = 3;
-    private static final int OUTLET_INDEX_TO_CUT = 0;
-    private static final double OUTLET_CONSUMPTION_VALUE = 1.0;
-    private static final double EXPECTED_CONSUMPTION_AFTER_CUT = 0.0;
-    private static final double EXPECTED_CONSUMPTION_BEFORE_CHECK = 10.0;
-    private static final double EXPECTED_CONSUMPTION_AFTER_CHECK = 2.0;
-    private static final double OUTLET_CONSUMPTION = 2.0;
 
     @Test
     void testGetOutlets() {
@@ -70,6 +62,8 @@ final class ElectricalMeterTest {
 
     @Test
     void testRemoveOutlet() {
+        final int outletIndexToRemove = 3;
+        final int expectedOutletListSizeAfterRemoval = 3;
         outlets.add(OutletFactory.cOutlet(new DeviceInfoImpl(new DeviceIdImpl(), COUTLET), STATE));
         outlets.add(OutletFactory.lOutlet(new DeviceInfoImpl(new DeviceIdImpl(), LOUTLET), STATE));
         outlets.add(OutletFactory.lOutlet(new DeviceInfoImpl(new DeviceIdImpl(), LOUTLET), STATE));
@@ -77,9 +71,9 @@ final class ElectricalMeterTest {
 
         final ElectricalMeter meter = new ElectricalMeterImpl(outlets);
         assertEquals(outlets.size(), meter.getOutlets().size());
-        Outlet toRemove = meter.getOutlets().get(OUTLET_INDEX_TO_REMOVE);
+        Outlet toRemove = meter.getOutlets().get(outletIndexToRemove);
         meter.removeOutlet(toRemove);
-        assertEquals(EXPECTED_OUTLET_LIST_SIZE, meter.getOutlets().size());
+        assertEquals(expectedOutletListSizeAfterRemoval, meter.getOutlets().size());
 
         Throwable removeOutletException = assertThrows(IllegalArgumentException.class, () -> {
             throw new IllegalArgumentException(toRemove.getInfo() + " not in 'outlets'");
@@ -90,19 +84,26 @@ final class ElectricalMeterTest {
 
     @Test
     void testCutPowerTo() {
+        final int outletIndexToCut = 0;
+        final double outletConsumptionValue = 1.0;
+        final double expectedConsumptionAfterCut = 0.0;
         outlets.add(OutletFactory.cOutlet(new DeviceInfoImpl(new DeviceIdImpl(), COUTLET), STATE));
 
         final ElectricalMeter meter = new ElectricalMeterImpl(outlets);
 
-        meter.getOutlets().get(OUTLET_INDEX_TO_CUT).setState(OUTLET_CONSUMPTION_VALUE);
+        meter.getOutlets().get(outletIndexToCut).setState(outletConsumptionValue);
 
-        meter.cutPowerTo(meter.getOutlets().get(OUTLET_INDEX_TO_CUT));
+        meter.cutPowerTo(meter.getOutlets().get(outletIndexToCut));
 
-        assertEquals(EXPECTED_CONSUMPTION_AFTER_CUT, meter.getOutlets().get(OUTLET_INDEX_TO_CUT).getState());
+        assertEquals(expectedConsumptionAfterCut, meter.getOutlets().get(outletIndexToCut).getState());
     }
 
     @Test
     void testCheckConsumption() {
+        final int expectedOutletListSize = 5;
+        final double outletConsumption = 1.5;
+        final double expectedConsumptionBeforeCheck = 7.5;
+        final double expectedConsumptionAfterCheck = 3.0;
 
         assertEquals(true, outlets.isEmpty());
         outlets.add(OutletFactory.cOutlet(new DeviceInfoImpl(new DeviceIdImpl(), COUTLET), STATE));
@@ -112,15 +113,15 @@ final class ElectricalMeterTest {
         outlets.add(OutletFactory.cOutlet(new DeviceInfoImpl(new DeviceIdImpl(), COUTLET), STATE));
 
         final ElectricalMeter meter = new ElectricalMeterImpl(outlets);
-        assertEquals(EXPECTED_OUTLET_LIST_SIZE, meter.getOutlets().size());
+        assertEquals(expectedOutletListSize, meter.getOutlets().size());
         for (Outlet outlet : meter.getOutlets()) {
-            outlet.setState(OUTLET_CONSUMPTION);
+            outlet.setState(outletConsumption);
         }
 
-        assertEquals(EXPECTED_CONSUMPTION_BEFORE_CHECK, meter.getGlobalConsumption());
+        assertEquals(expectedConsumptionBeforeCheck, meter.getGlobalConsumption());
 
         meter.checkConsumption();
-        assertEquals(EXPECTED_CONSUMPTION_AFTER_CHECK, meter.getGlobalConsumption());
+        assertEquals(expectedConsumptionAfterCheck, meter.getGlobalConsumption());
 
     }
 }
