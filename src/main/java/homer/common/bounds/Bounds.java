1413742b3c3504795be44d33ced44b5343329e48
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-03-16 21:00:46 +0100

    [bounds] add Bounds

---
+++ src/main/java/homer/common/bounds/Bounds.java
@@ -0,0 +1,42 @@
+package homer.common.bounds;
+
+/**
+ * This class encapsulates the concept of numerical boundaries.
+ * 
+ * @param <S> The comparable number type.
+ */
+public final class Bounds<N extends Number & Comparable<N>> {
+
+    private final N lower;
+    private final N upper;
+
+    /**
+     * Constructs a pair of bounds.
+     * 
+     * @param lower the lower boundary value.
+     * @param upper the upper boundary value.
+     */
+    public Bounds(final N lower, final N upper) {
+        this.lower = lower;
+        this.upper = upper;
+    }
+
+    /**
+     * Returns the lower boundary value.
+     * 
+     * @return the lower boundary value.
+     */
+    public N getLower() {
+        return this.lower;
+    }
+
+    /**
+     * Returns the upper boundary value.
+     * 
+     * @return the upper boundary value.
+     */
+    public N getUpper() {
+        return this.upper;
+    }
+
+}
