148a7053ca79ef48f85ff8b41ce2c63ae471c5e6
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-03-16 15:48:30 +0100

    [limit] rename upperLimit to upperBound in TestLimit

--- src/test/java/homer/common/limit/TestLimit.java
+++ src/test/java/homer/common/limit/TestLimit.java
@@ -12,49 +12,49 @@ final class TestLimit {
     @Test
     void testLimitInt() {
         final int lowerBound = 10;
-        final int upperLimit = 30;
+        final int upperBound = 30;
         final int belowRange = lowerBound - 10;
         final int inRange = 20;
-        final int aboveRange = upperLimit + 10;
-        assertEquals(lowerBound, Limit.limit(belowRange, lowerBound, upperLimit));
-        assertEquals(inRange, Limit.limit(inRange, lowerBound, upperLimit));
-        assertEquals(upperLimit, Limit.limit(aboveRange, lowerBound, upperLimit));
+        final int aboveRange = upperBound + 10;
+        assertEquals(lowerBound, Limit.limit(belowRange, lowerBound, upperBound));
+        assertEquals(inRange, Limit.limit(inRange, lowerBound, upperBound));
+        assertEquals(upperBound, Limit.limit(aboveRange, lowerBound, upperBound));
     }
 
     @Test
     void testLimitDouble() {
         final double lowerBound = 10.5d;
-        final double upperLimit = 30.5d;
+        final double upperBound = 30.5d;
         final double belowRange = lowerBound - 10.0d;
         final double inRange = 24.5d;
-        final double aboveRange = upperLimit + 10.0d;
-        assertEquals(lowerBound, Limit.limit(belowRange, lowerBound, upperLimit));
-        assertEquals(inRange, Limit.limit(inRange, lowerBound, upperLimit));
-        assertEquals(upperLimit, Limit.limit(aboveRange, lowerBound, upperLimit));
+        final double aboveRange = upperBound + 10.0d;
+        assertEquals(lowerBound, Limit.limit(belowRange, lowerBound, upperBound));
+        assertEquals(inRange, Limit.limit(inRange, lowerBound, upperBound));
+        assertEquals(upperBound, Limit.limit(aboveRange, lowerBound, upperBound));
     }
 
     @Test
     void testLimitFloat() {
         final float lowerBound = 10.0f;
-        final float upperLimit = 30.0f;
+        final float upperBound = 30.0f;
         final float belowRange = lowerBound - 10.0f;
         final float inRange = 20.0f;
-        final float aboveRange = upperLimit + 10.0f;
-        assertEquals(lowerBound, Limit.limit(belowRange, lowerBound, upperLimit));
-        assertEquals(inRange, Limit.limit(inRange, lowerBound, upperLimit));
-        assertEquals(upperLimit, Limit.limit(aboveRange, lowerBound, upperLimit));
+        final float aboveRange = upperBound + 10.0f;
+        assertEquals(lowerBound, Limit.limit(belowRange, lowerBound, upperBound));
+        assertEquals(inRange, Limit.limit(inRange, lowerBound, upperBound));
+        assertEquals(upperBound, Limit.limit(aboveRange, lowerBound, upperBound));
     }
 
     @Test
     void testLimitBigInt() {
         final var lowerBound = BigInteger.valueOf(-100);
-        final var upperLimit = BigInteger.valueOf(100);
+        final var upperBound = BigInteger.valueOf(100);
         final var belowRange = lowerBound.add(BigInteger.valueOf(-100));
         final var inRange = BigInteger.valueOf(50);
-        final var aboveRange = upperLimit.add(BigInteger.valueOf(100));
-        assertEquals(lowerBound, Limit.limit(belowRange, lowerBound, upperLimit));
-        assertEquals(inRange, Limit.limit(inRange, lowerBound, upperLimit));
-        assertEquals(upperLimit, Limit.limit(aboveRange, lowerBound, upperLimit));
+        final var aboveRange = upperBound.add(BigInteger.valueOf(100));
+        assertEquals(lowerBound, Limit.limit(belowRange, lowerBound, upperBound));
+        assertEquals(inRange, Limit.limit(inRange, lowerBound, upperBound));
+        assertEquals(upperBound, Limit.limit(aboveRange, lowerBound, upperBound));
     }
 
     @Test
