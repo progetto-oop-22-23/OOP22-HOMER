be4a9e9b96146a74f5bbf443d1b5da6493c498b1
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-03-16 19:12:49 +0100

    [limit] remove limit methods for primitive types
    While having overloaded methods with primitive type parameters might have a performance advantage due to not requiring boxing, it is deemed better to manage a single method at this moment

--- src/main/java/homer/common/limit/Limit.java
+++ src/main/java/homer/common/limit/Limit.java
@@ -14,32 +14,6 @@ public final class Limit {
     /**
      * Limits the input between lower and upper bounds.
      * 
-     * @param input      The input to limit.
-     * @param lowerBound The lower bound.
-     * @param upperBound The upper bound.
-     * @return The input limited between the lower and upper bounds.
-     */
-    public static int limit(final int input, final int lowerBound, final int upperBound) {
-        checkBoundsOrder(lowerBound, upperBound);
-        return Math.max(lowerBound, Math.min(input, upperBound));
-    }
-
-    /**
-     * Limits the input between lower and upper bounds.
-     * 
-     * @param input      The input to limit.
-     * @param lowerBound The lower bound.
-     * @param upperBound The upper bound.
-     * @return The input limited between the lower and upper bounds.
-     */
-    public static double limit(final double input, final double lowerBound, final double upperBound) {
-        checkBoundsOrder(lowerBound, upperBound);
-        return Math.max(lowerBound, Math.min(input, upperBound));
-    }
-
-    /**
-     * Limits the input between lower and upper bounds.
-     * 
      * @param <S>        The comparable type.
      * @param input      The input to limit.
      * @param lowerBound The lower bound.
