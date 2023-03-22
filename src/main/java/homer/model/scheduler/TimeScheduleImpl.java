539caba1406f529ae7d1e98d423961b7d7357aa6
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-03-22 08:23:08 +0100

    [scheduler] add TimeScheduleImpl

---
+++ src/main/java/homer/model/scheduler/TimeScheduleImpl.java
@@ -0,0 +1,13 @@
+package homer.model.scheduler;
+
+import java.time.LocalTime;
+
+public class TimeScheduleImpl<T> implements TimeSchedule<T> {
+
+    @Override
+    public void checkSchedule(LocalTime currentTime, T currentParameter) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'checkSchedule'");
+    }
+
+}
