10515fe6fb932a653e200c24b0b5cba48379473f
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-03-22 15:52:15 +0100

    [TEST] Unit tests for setState()

--- src/test/java/homer/model/outlets/OutletTest.java
+++ src/test/java/homer/model/outlets/OutletTest.java
@@ -1,9 +1,13 @@
 package homer.model.outlets;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.Test;
+import java.time.Duration;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import homer.DeviceInfoImpl;
 import homer.api.DeviceIdImpl;
@@ -16,6 +20,7 @@ final class OutletTest {
     void testSetState() {
         final double expectedCoutletState = 1.0;
         final double expectedLoutletState = 2.5;
+        final int milliseconds = 6;
 
         final Outlet cOutlet = OutletFactory.cOutlet(new DeviceInfoImpl(new DeviceIdImpl(), "COUTLET"), 0);
         final Outlet lOutlet = OutletFactory.lOutlet(new DeviceInfoImpl(new DeviceIdImpl(), "LOUTLET"), 0);
@@ -23,6 +28,15 @@ final class OutletTest {
         assertEquals(expectedCoutletState, cOutlet.getState());
         lOutlet.setState(expectedLoutletState);
         assertEquals(expectedLoutletState, lOutlet.getState());
+
+        final Light light = new Light(new DeviceInfoImpl(new DeviceIdImpl(), "LIGHT"), true,
+                new PoweredDeviceInfoImpl(10.0, lOutlet));
+        lOutlet.setState(0.0);
+        lOutlet.plug(light);
+        light.updateTick(Duration.ofMillis(milliseconds));
+        lOutlet.setState();
+        assertTrue(lOutlet.getState() < lOutlet.getMaxValue());
+        assertEquals(light.getInstantConsumption(), lOutlet.getState());
     }
 
     @Test
