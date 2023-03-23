5b990c6325dfb98708df80d0ecbfdf6065f97746
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-03-23 13:24:03 +0100

    [TEST_OUTLET] Split testSetState and testUpdateTick

--- src/test/java/homer/model/outlets/OutletTest.java
+++ src/test/java/homer/model/outlets/OutletTest.java
@@ -14,10 +14,9 @@ import homer.model.lights.Light;
 final class OutletTest {
 
     @Test
-    void testUpdateTickOneHour() {
+    void testSetState() {
         final double expectedCoutletState = 1.0;
         final double expectedLoutletState = 2.5;
-        final int milliseconds = 8;
 
         final Outlet cOutlet = OutletFactory.cOutlet(new DeviceInfoImpl(new DeviceIdImpl(), "COUTLET"), 0);
         final Outlet lOutlet = OutletFactory.lOutlet(new DeviceInfoImpl(new DeviceIdImpl(), "LOUTLET"), 0);
@@ -25,14 +24,20 @@ final class OutletTest {
         assertEquals(expectedCoutletState, cOutlet.getState());
         lOutlet.setState(expectedLoutletState);
         assertEquals(expectedLoutletState, lOutlet.getState());
+    }
 
+    @Test
+    void testUpdateTickOneHour() {
+        final int milliseconds = 8;
+
+        final Outlet lOutlet = OutletFactory.lOutlet(new DeviceInfoImpl(new DeviceIdImpl(), "LOUTLET"), 0);
         final Light light = new Light(new DeviceInfoImpl(new DeviceIdImpl(), "LIGHT"), true,
                 new PoweredDeviceInfoImpl(10.0, lOutlet));
         lOutlet.setState(0.0);
         lOutlet.plug(light);
         light.updateTick(Duration.ofMillis(milliseconds));
         lOutlet.updateTick(Duration.ofHours(1));
-        
+
         assertTrue(lOutlet.getState() > lOutlet.getMinValue());
         assertTrue(lOutlet.getState() < lOutlet.getMaxValue());
     }
