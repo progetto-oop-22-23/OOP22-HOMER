a13bfdb31efa1c7944b86ff112168b19823f0993
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-03-23 18:42:24 +0100

    [thermometer] refactor Thermometer with TemperatureObserver

--- src/main/java/homer/model/thermometer/Thermometer.java
+++ src/main/java/homer/model/thermometer/Thermometer.java
@@ -2,10 +2,11 @@ package homer.model.thermometer;
 
 import homer.api.Device;
 import homer.common.Temperature;
+import homer.model.environment.TemperatureObserver;
 
 /**
  * This interface models a thermometer {@link Device}.
  */
-public interface Thermometer extends Device<Temperature> {
+public interface Thermometer extends Device<Temperature>, TemperatureObserver {
 
 }
