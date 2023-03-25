d65f8bca5cd390b4d48eb4d7f511522c44279774
Michele Ceccacci <michele.ceccacci@studio.unibo.it>
2023-03-25 22:05:08 +0100

    [view] refactor sliderComponent

--- src/main/java/homer/view/JFXApplication.java
+++ src/main/java/homer/view/JFXApplication.java
@@ -1,8 +1,11 @@
 package homer.view;
 
+import homer.api.DeviceIdImpl;
 import homer.controller.Controller;
 import homer.controller.ControllerImpl;
+import homer.model.temperaturechangers.TemperatureChangerState;
 import homer.view.javafx.AddDevicesView;
+import homer.view.javafx.TemperatureChangerView;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -17,8 +20,9 @@ public class JFXApplication extends Application {
         Group root = new Group();
         Scene scene = new Scene(root, 300, 500);
         VBox vBox = new VBox();
+        final var state = new TemperatureChangerState().addCurrentIntensity(3.0).addMaxIntensity(10).addMinIntensity(1);
         Controller controller = new ControllerImpl();
-        root.getChildren().addAll(vBox, new AddDevicesView(controller));
+        root.getChildren().addAll(vBox, new AddDevicesView(controller), new TemperatureChangerView(new DeviceIdImpl(), state, controller));
         stage.setTitle("demo");
         stage.setScene(scene);
         stage.show();
