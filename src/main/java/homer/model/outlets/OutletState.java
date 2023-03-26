977bdedcbf8ce17ab7be849da93169e70ce73b9b
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-03-26 01:11:17 +0100

    [OUTLET_STATE] Implement methods

--- src/main/java/homer/model/outlets/OutletState.java
+++ src/main/java/homer/model/outlets/OutletState.java
@@ -1,7 +1,38 @@
 package homer.model.outlets;
 
+import java.util.Optional;
+
 import homer.api.DeviceState;
 
 public class OutletState implements DeviceState {
-    
+    Optional<Double> minPower = Optional.empty();
+    Optional<Double> maxPower = Optional.empty();
+    Optional<Double> power = Optional.empty();
+
+    public Optional<Double> getMin() {
+        return this.minPower;
+    }
+
+    public Optional<Double> getMax() {
+        return this.maxPower;
+    }
+
+    public Optional<Double> getPower() {
+        return this.power;
+    }
+
+    OutletState addMin(double minPower) {
+        this.minPower = Optional.of(minPower);
+        return this;
+    }
+
+    OutletState addMax(double maxPower) {
+        this.maxPower = Optional.of(maxPower);
+        return this;
+    }
+
+    OutletState addValue(double value) {
+        this.power = Optional.of(value);
+        return this;
+    }
 }
