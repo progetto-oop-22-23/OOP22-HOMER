e4a5c006cb683568208789312a2cb31d068887a8
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-03-27 21:15:00 +0200

    [VIEW_METER] First demo
    Adding/removing outlets and displaying consumption dynamically

--- src/main/java/homer/view/javafx/ElectricalMeterView.java
+++ src/main/java/homer/view/javafx/ElectricalMeterView.java
@@ -5,16 +5,82 @@ import java.util.List;
 import homer.controller.api.electricalmeter.ElectricalMeter;
 import homer.controller.impl.electricalmeter.ElectricalMeterImpl;
 import homer.model.outlets.Outlet;
+import homer.model.outlets.OutletFactory;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ElectricalMeterView extends Application {
 
     List<Outlet> outlets = List.of();
     private final ElectricalMeter meter = new ElectricalMeterImpl(outlets);
+
+    protected static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        GridPane gridPane = new GridPane();
+        Label powerConsumptionLabel = new Label();
+        Label energyAbsorptionLabel = new Label();
+        Label[] outletLabels = new Label[meter.getOutlets().size() + 1];
+
+        for (int i = 0; i < outletLabels.length; i++) {
+            outletLabels[i] = new Label("Outlet " + (i + 1) + " consumption: ");
+        }
+
+        Button addOutlet = new Button("+ Add Outlet");
+        addOutlet.setOnAction(event -> {
+            meter.addOutlet(OutletFactory.cOutlet(10.0));
+            meter.computeConsumption();
+            powerConsumptionLabel.setText("Power consumption: " + meter.getGlobalConsumption() + " kW");
+        energyAbsorptionLabel.setText("Energy absorption: " + meter.getAveragePower() + " kW/h");
+            outletLabels[outletLabels.length - 1].setText("Outlet " + outletLabels.length + " consumption: ");
+        });
+
+        Button removeOutlet = new Button("- Remove Outlet");
+        removeOutlet.setOnAction(event -> {
+            Outlet toRemove = meter.getOutlets().get(0);
+            if (toRemove instanceof Outlet) {
+                meter.removeOutlet(meter.getOutlets().get(0));
+                outletLabels[0].setText("");
+            }
+        });
+
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+        gridPane.setVgap(10);
+        gridPane.setHgap(10);
+
+        //gridPane.add(powerConsumptionLabel, 0, 0);
+        //gridPane.add(energyAbsorptionLabel, 1, 0);
+
+        for (int i = 0; i < outletLabels.length - 1; i++) {
+            gridPane.add(outletLabels[i], 0, i + 1);
+        }
+
+        VBox buttonBox = new VBox(10, addOutlet, removeOutlet);
+        buttonBox.setAlignment(Pos.CENTER);
+        gridPane.add(buttonBox, 2, 1, 1, outletLabels.length);
+
+        // Add labels for the meter's power consumption and energy absorption
+       /* Label meterPowerLabel = new Label();
+        Label meterEnergyLabel = new Label(); */
+
         
+        gridPane.add(powerConsumptionLabel, 0, 0);
+        gridPane.add(energyAbsorptionLabel, 0, 1);
+        Scene scene = new Scene(gridPane, 400, 400);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Electrical Meter");
+        primaryStage.show();
     }
-    
+
 }
