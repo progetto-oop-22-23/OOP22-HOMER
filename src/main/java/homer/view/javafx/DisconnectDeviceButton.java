df76b615878b98d0fd3240e5c99a15ce2119425c
Michele Ceccacci <michele.ceccacci@studio.unibo.it>
2023-03-27 18:32:26 +0200

    [view] refactor SliderComponent and DisconnectDeviceButton

--- src/main/java/homer/view/javafx/DisconnectDeviceButton.java
+++ src/main/java/homer/view/javafx/DisconnectDeviceButton.java
@@ -6,10 +6,15 @@ import homer.controller.command.DisconnectDevice;
 import javafx.scene.control.Button;
 
 /**
- * Disconnects the device from the view.
+ * Disconnects the device from the view on click.
  */
 public final class DisconnectDeviceButton extends Button {
 
+    /**
+     * 
+     * @param controller
+     * @param deviceId
+     */
     public DisconnectDeviceButton(final Controller controller, final DeviceId deviceId) {
         this.setText("Remove Device");
         this.setOnMouseClicked(e -> controller.receiveCommand(new DisconnectDevice(deviceId))); 
