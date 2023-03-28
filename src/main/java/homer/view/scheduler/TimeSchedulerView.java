70b7152ce78436b0917a00c30e2956ce44e37d52
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-03-28 22:16:15 +0200

    [scheduler] update javadoc

--- src/main/java/homer/view/scheduler/TimeSchedulerView.java
+++ src/main/java/homer/view/scheduler/TimeSchedulerView.java
@@ -6,12 +6,28 @@ import java.util.UUID;
 import homer.controller.scheduler.TimeSchedulerController;
 import homer.model.scheduler.TimeSchedule;
 
+/**
+ * View for the time scheduler.
+ * 
+ * @param <T> the type of parameter to target for the schedule.
+ */
 public interface TimeSchedulerView<T extends Comparable<T>> {
 
+    /**
+     * Sends an updated collection of schedules currently active to the view which
+     * redraws them all.
+     * 
+     * @param schedules the map containing the schedules and their ids.
+     */
     void updateSchedules(Map<UUID, TimeSchedule<T>> schedules);
 
     void removeSchedule(UUID scheduleId);
 
+    /**
+     * Sets the scheduler controller.
+     * 
+     * @param scheduler the scheduler controller.
+     */
     void setScheduler(TimeSchedulerController<T> scheduler);
 
 }
