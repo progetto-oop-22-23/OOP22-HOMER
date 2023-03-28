7d9f99fbfba2797ebd0e27dd3eaf3fbd6366f7a5
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-03-28 18:09:13 +0200

    [simview] convert SimManagerViewFxImpl to a HBox

--- src/main/java/homer/view/sim/SimManagerViewFxImpl.java
+++ src/main/java/homer/view/sim/SimManagerViewFxImpl.java
@@ -3,18 +3,14 @@ package homer.view.sim;
 import java.time.Duration;
 
 import homer.core.SimManagerViewObserver;
-import javafx.application.Platform;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
 
 /**
  * Implementation of {@link SimManagerView} using JavaFX.
  */
-public final class SimManagerViewFxImpl extends Stage implements SimManagerView {
+public final class SimManagerViewFxImpl extends HBox implements SimManagerView {
 
-    private static final String TITLE = "Simulation Manager";
     private static final String RESUME = "Resume";
     private static final String PAUSE = "Pause";
     private static final String ONE_SEC = "1s";
@@ -25,26 +21,14 @@ public final class SimManagerViewFxImpl extends Stage implements SimManagerView 
     private SimManagerViewObserver simManager;
 
     /**
-     * Creates a new {@link SimManagerViewFxImpl} and displays it.
+     * Creates a new {@link SimManagerViewFxImpl} JavaFX HBox.
      */
     public SimManagerViewFxImpl() {
-        this.setOnCloseRequest(event -> {
-            Platform.exit();
-            System.exit(0);
-        });
         final Button resume = new Button(RESUME);
         final Button pause = new Button(PAUSE);
         final Button oneSec = new Button(ONE_SEC);
         final Button oneMin = new Button(ONE_MIN);
         final Button oneHour = new Button(ONE_HOUR);
-        final HBox root = new HBox();
-        root.getChildren().add(resume);
-        root.getChildren().add(pause);
-        root.getChildren().add(oneSec);
-        root.getChildren().add(oneMin);
-        root.getChildren().add(oneHour);
-        // this.setScene(new Scene(root, 300, 300));
-        this.setScene(new Scene(root));
 
         resume.setOnAction(event -> simManager.resume());
         pause.setOnAction(event -> simManager.pause());
@@ -52,9 +36,7 @@ public final class SimManagerViewFxImpl extends Stage implements SimManagerView 
         oneMin.setOnAction(event -> simManager.setSimStepPeriod(Duration.ofMinutes(1)));
         oneHour.setOnAction(event -> simManager.setSimStepPeriod(Duration.ofHours(1)));
 
-        this.sizeToScene();
-        this.setTitle(TITLE);
-        this.show();
+        // this.getChildren().addAll(resume, pause, oneSec, oneMin, oneHour);
     }
 
     @Override
