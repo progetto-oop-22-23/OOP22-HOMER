3d62b305b4e2e52e94089b7ddcbc65585a2f83f0
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-03-28 18:14:48 +0200

    [view] refactor view with borderpane and tabs

--- src/main/java/homer/view/JFXApplication.java
+++ src/main/java/homer/view/JFXApplication.java
@@ -3,13 +3,18 @@ package homer.view;
 import homer.controller.Controller;
 import homer.controller.ControllerImpl;
 import homer.view.javafx.AddDevicesView;
+import homer.view.logger.Logger;
+import homer.view.logger.LoggerImpl;
 import homer.core.SimManagerImpl;
 import homer.view.sim.SimManagerViewFxImpl;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TabPane.TabClosingPolicy;
+import javafx.scene.control.TabPane.TabDragPolicy;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
@@ -27,21 +32,54 @@ public class JFXApplication extends Application {
             System.exit(0);
         });
 
+        final var root = new BorderPane();
         final Scene scene = new Scene(root, INITIAL_W, INITIAL_H);
-        final VBox vBox = new VBox();
         final Controller controller = new ControllerImpl();
-        root.getChildren().addAll(vBox, new AddDevicesView(controller));
 
         final var simManager = new SimManagerImpl(controller);
+        final var simView = new SimManagerViewFxImpl();
         Platform.runLater(() -> {
-            final var simView = new SimManagerViewFxImpl();
             simView.setObserver(simManager);
         });
+
+        final var viewManager = controller.getViewManager();
+        final Logger logger = new LoggerImpl(null);
+        viewManager.addView(logger);
+
+        // CREATE MAIN WINDOW
+        // add tabs:
+        // - device viewer
+        // (device widgets which include the remove button) with add device section
+        // - scheduler
+        // - graphs
+
+        // TODO display the sim world time somewhere (top/bottom)
+
+        final TabPane tabPane = new TabPane();
+        tabPane.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
+        tabPane.setTabDragPolicy(TabDragPolicy.REORDER);
+
+        // TODO add dashboard
+        final Tab devicesView = new Tab();
+        devicesView.setText("DEVICES");
+        devicesView.setContent(new AddDevicesView(controller));
+
+        final Tab schedulerView = new Tab();
+        schedulerView.setText("SCHEDULER");
+        // schedulerView.setContent(null); // TODO
+
+        final Tab graphView = new Tab();
+        graphView.setText("GRAPHS");
+        // graphView.setContent(null); // TODO
+
+        tabPane.getTabs().addAll(devicesView, schedulerView, graphView);
+
+        root.setCenter(tabPane);
+        root.setBottom(simView);
 
         stage.setTitle(TITLE);
         stage.setScene(scene);
         stage.show();
     }
-
 
 }
