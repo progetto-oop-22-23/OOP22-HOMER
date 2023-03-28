00dc5359049b03cc6f6c47d4c32553e14bc29915
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-03-28 22:07:48 +0200

    [scheduler] convert TimeSchedule from interface to record

--- src/main/java/homer/model/scheduler/TimeSchedule.java
+++ src/main/java/homer/model/scheduler/TimeSchedule.java
@@ -5,15 +5,10 @@ import java.time.LocalTime;
 import homer.common.bounds.Bounds;
 
 /**
- * This interface models a constraint on a certain parameter that should
+ * Represents a constraint on a certain parameter that should
  * be met in a certain time interval.
  * 
  * @param <T> The parameter that should meet the constraint.
  */
-public interface TimeSchedule<T extends Comparable<T>> {
-
-    Bounds<LocalTime> getTimeBounds();
-
-    Bounds<T> getParaBounds();
-
+public record TimeSchedule<T extends Comparable<T>>(Bounds<LocalTime> timeBounds, Bounds<T> paramBounds) {
 }
