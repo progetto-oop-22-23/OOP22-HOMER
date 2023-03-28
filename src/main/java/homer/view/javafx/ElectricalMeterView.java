91de968b29aa519c0c2efa4b7b0c5a6ae921e557
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-03-29 00:21:35 +0200

    [VIEW_METER] Automatic view update
    JUST FOR TESTING, WILL BE CHANGED

--- src/main/java/homer/view/javafx/ElectricalMeterView.java
+++ src/main/java/homer/view/javafx/ElectricalMeterView.java
@@ -4,40 +4,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 import homer.controller.api.electricalmeter.ElectricalMeter;
+import homer.controller.impl.electricalmeter.ElectricalMeterImpl;
 import homer.model.outlets.Outlet;
-import javafx.beans.binding.Bindings;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.util.Duration;
 
+/**
+ * View for {@link homer.controller.impl.electricalmeter}.
+ */
 public class ElectricalMeterView extends BorderPane {
 
     private ElectricalMeter meter;
     private TableView<List<Outlet>> outletTable;
     private TableColumn<List<Outlet>, Double> outletStatusColumn;
-    private TableColumn<List<Outlet>, Void> outletActionColumn;
+    //private TableColumn<List<Outlet>, Void> outletActionColumn;
     private Label globalConsumptionLabel;
     private Label averagePowerLabel;
 
+    /**
+     * View constructor
+     * @param meter the electrical meter.
+     */
     public ElectricalMeterView(ElectricalMeter meter) {
         this.meter = meter;
         initView();
     }
 
+    /**
+     * Initializes and builds the view.
+     */
     private void initView() {
-
         this.outletTable = new TableView<>();
         ObservableList<List<Outlet>> outlets = FXCollections.observableArrayList();
-        final int outletTableMaxColumns = 3;
-        final int outletTableRowHeightOffset = 30;
-        final int outletTableHeight = outlets.size() * outletTableRowHeightOffset;
-        final int outletTableDeltaHeight = 34;
 
         for (Outlet outlet : meter.getOutlets()) {
             List<Outlet> outletList = new ArrayList<>();
@@ -60,14 +70,17 @@ public class ElectricalMeterView extends BorderPane {
                 if (empty) {
                     setText(null);
                 } else {
-                    Outlet outlet = getTableRow().getItem().get(0);
-                    Double power = outlet.getState().getPower().get();
-                    setText(String.format("%.2f", power));
+                    TableRow<List<Outlet>> row = getTableRow();
+                    if (row != null) {
+                        Outlet outlet = row.getItem().get(0);
+                        Double power = outlet.getState().getPower().get();
+                        setText(String.format("%.2f W", power));
+                    }
                 }
             }
         });
 
-        outletActionColumn = new TableColumn<>("Action");
+        /*outletActionColumn = new TableColumn<>("Action");
         outletActionColumn.setCellFactory(param -> new TableCell<List<Outlet>, Void>() {
             private final ToggleButton outletSwitch = new ToggleButton("Switch OFF");
             {
@@ -89,24 +102,70 @@ public class ElectricalMeterView extends BorderPane {
                 }
             }
         });
-
+*/
         List<TableColumn<List<Outlet>, ?>> columns = new ArrayList<>();
         columns.add(outletStatusColumn);
-        columns.add(outletActionColumn);
+       // columns.add(outletActionColumn);
 
-        outletTable.getColumns().addAll(columns.subList(0, Math.min(columns.size(), outletTableMaxColumns)));
+        final int outletTableMinColumns = 1;
+        final int outletTableRowHeightOffset = 60;
+        final int outletTableHeight = outlets.size() * outletTableRowHeightOffset;
+        final int outletTableDeltaHeight = 90;
+
+        outletTable.getColumns().addAll(columns.subList(0, Math.min(columns.size(), outletTableMinColumns)));
         outletTable.setPrefHeight(outletTableHeight + outletTableDeltaHeight);
 
         globalConsumptionLabel = new Label("Global Consumption: ");
-        globalConsumptionLabel.textProperty()
-                .bind(Bindings.format("Global Consumption: %.2f W", meter.getGlobalConsumption()));
+        globalConsumptionLabel.setText("Global Consumption: " + meter.getGlobalConsumption() + "W");
 
         // Initialize average power label
         averagePowerLabel = new Label("Average Power: ");
-        averagePowerLabel.textProperty().bind(Bindings.format("Average Power: %.2f Wh", meter.getAveragePower()));
+        averagePowerLabel.setText("Average Power: " + meter.getAveragePower() + "Wh");
 
+        Timeline updateTimeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+            ((ElectricalMeterImpl) meter).updateTick(java.time.Duration.ofHours(13));
+            updateUI();
+        }));
+        // Create a ToggleButton to start and stop the update timeline
+        ToggleButton updateButton = new ToggleButton("Start Update");
+        updateButton.setOnAction(event -> {
+            if (updateButton.isSelected()) {
+                updateTimeline.play();
+                updateButton.setText("Stop Update");
+            } else {
+                updateTimeline.pause();
+                updateButton.setText("Start Update");
+            }
+        });
+        updateTimeline.setCycleCount(Animation.INDEFINITE);
+        // Add the ToggleButton to the UI
+        this.setTop(new HBox(outletTable));
         // Set up view
         this.setTop(outletTable);
-        this.setBottom(new HBox(globalConsumptionLabel, averagePowerLabel));
+        this.setBottom(new HBox(globalConsumptionLabel, averagePowerLabel, updateButton));
+    }
+
+    /**
+     * Updates the view.
+     */
+    public void updateUI() {
+        averagePowerLabel.setText("Average Power: " + meter.getAveragePower() + "Wh");
+        globalConsumptionLabel.setText("Global Consumption: " + meter.getGlobalConsumption() + "W");
+        outletStatusColumn.setCellFactory(param -> new TableCell<List<Outlet>, Double>() {
+            @Override
+            protected void updateItem(Double item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setText(null);
+                } else {
+                    TableRow<List<Outlet>> row = getTableRow();
+                    if (row != null) {
+                        Outlet outlet = row.getItem().get(0);
+                        Double power = outlet.getState().getPower().get();
+                        setText(String.format("%.2f W", power));
+                    }
+                }
+            }
+        });
     }
 }
