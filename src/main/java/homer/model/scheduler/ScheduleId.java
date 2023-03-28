f3cc6b90e79aa13ebcef0dc597875845f68f4771
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-03-28 22:27:58 +0200

    [scheduler] update javadoc

--- src/main/java/homer/model/scheduler/ScheduleId.java
+++ src/main/java/homer/model/scheduler/ScheduleId.java
@@ -4,6 +4,8 @@ import java.util.UUID;
 
 /**
  * Represents an unique id for a schedule.
+ * 
+ * @param scheduleId the schedule id.
  */
 public record ScheduleId(UUID scheduleId) {
 }
--- src/main/java/homer/model/scheduler/TimeSchedule.java
+++ src/main/java/homer/model/scheduler/TimeSchedule.java
@@ -8,7 +8,9 @@ import homer.common.bounds.Bounds;
  * Represents a constraint on a certain parameter that should
  * be met in a certain time interval.
  * 
- * @param <T> The parameter that should meet the constraint.
+ * @param timeBounds  the time interval.
+ * @param paramBounds the target parameter interval.
+ * @param <T>         The parameter that should meet the constraint.
  */
 public record TimeSchedule<T extends Comparable<T>>(Bounds<LocalTime> timeBounds, Bounds<T> paramBounds) {
 }
