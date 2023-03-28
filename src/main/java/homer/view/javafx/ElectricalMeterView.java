edaf4b8eca70fc7578eb0331d24bca19a113d496
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-03-28 22:56:59 +0200

    [VIEW_METER] Remove magic numbers

--- src/main/java/homer/view/javafx/ElectricalMeterView.java
+++ src/main/java/homer/view/javafx/ElectricalMeterView.java
@@ -18,22 +18,22 @@ import javafx.scene.layout.HBox;
 
 public class ElectricalMeterView extends BorderPane {
 
-    private ElectricalMeter controller;
+    private ElectricalMeter meter;
     private TableView<List<Outlet>> outletTable;
     private TableColumn<List<Outlet>, Double> outletStatusColumn;
     private TableColumn<List<Outlet>, Void> outletActionColumn;
     private Label globalConsumptionLabel;
     private Label averagePowerLabel;
 
-    public ElectricalMeterView(ElectricalMeter controller) {
-        this.controller = controller;
+    public ElectricalMeterView(ElectricalMeter meter) {
+        this.meter = meter;
         initView();
     }
 
     private void initView() {
         this.outletTable = new TableView<>();
         ObservableList<List<Outlet>> outlets = FXCollections.observableArrayList();
-        for (Outlet outlet : controller.getOutlets()) {
+        for (Outlet outlet : meter.getOutlets()) {
             List<Outlet> outletList = new ArrayList<>();
             outletList.add(outlet);
             outlets.add(outletList);
@@ -63,11 +63,11 @@ public class ElectricalMeterView extends BorderPane {
 
         outletActionColumn = new TableColumn<>("Action");
         outletActionColumn.setCellFactory(param -> new TableCell<List<Outlet>, Void>() {
-            private final ToggleButton outletSwitch = new ToggleButton();
+            private final ToggleButton outletSwitch = new ToggleButton("Switch OFF");
             {
                 outletSwitch.setOnAction(event -> {
                     List<Outlet> outlet = getTableView().getItems().get(getIndex());
-                    controller.cutPowerTo(outlet.get(0));
+                    meter.cutPowerTo(outlet.get(0));
                 });
             }
 
@@ -88,20 +88,20 @@ public class ElectricalMeterView extends BorderPane {
         columns.add(outletStatusColumn);
         columns.add(outletActionColumn);
 
-        outletTable.getColumns().addAll(columns.subList(0, Math.min(columns.size(), 3)));
-        outletTable.setPrefHeight(outlets.size() * 30 + 34);
+        final int maxColumns = 3;
+        final int offset = 30;
+        final int height = outlets.size() * offset;
+        final int deltaHeight = 34;
+        outletTable.getColumns().addAll(columns.subList(0, Math.min(columns.size(), maxColumns)));
+        outletTable.setPrefHeight(height + deltaHeight);
 
-        // outletTable.getColumns().addAll(outletNameColumn, outletStatusColumn,
-        // outletActionColumn);
-
-        // Initialize global consumption label
         globalConsumptionLabel = new Label("Global Consumption: ");
         globalConsumptionLabel.textProperty()
-                .bind(Bindings.format("Global Consumption: %.2f W", controller.getGlobalConsumption()));
+                .bind(Bindings.format("Global Consumption: %.2f W", meter.getGlobalConsumption()));
 
         // Initialize average power label
         averagePowerLabel = new Label("Average Power: ");
-        averagePowerLabel.textProperty().bind(Bindings.format("Average Power: %.2f Wh", controller.getAveragePower()));
+        averagePowerLabel.textProperty().bind(Bindings.format("Average Power: %.2f Wh", meter.getAveragePower()));
 
         // Set up view
         this.setTop(outletTable);
