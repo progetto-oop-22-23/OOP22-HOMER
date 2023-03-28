ec1c295689131b23941f1a2cfefb18daee2af80a
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-03-29 00:37:35 +0200

    [scheduler] add test for TemperatureScheduler

---
+++ src/test/java/homer/model/scheduler/TestTemperatureScheduler.java
@@ -0,0 +1,88 @@
+package homer.model.scheduler;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalTime;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import homer.common.bounds.Bounds;
+import homer.common.temperature.Temperature;
+import homer.common.temperature.TemperatureFactory;
+import homer.model.scheduler.TimeScheduler.ParameterResult;
+
+final class TestTemperatureScheduler {
+
+    private static final Bounds<LocalTime> TIME_RANGE = new Bounds<>(LocalTime.of(9, 0), LocalTime.of(12, 0));
+    private static final Bounds<Temperature> TEMP_RANGE = new Bounds<>(TemperatureFactory.fromCelsius(19),
+            TemperatureFactory.fromCelsius(22));
+    private static final LocalTime TIME_EARLY = LocalTime.of(0, 0);
+    private static final LocalTime TIME_IN_RANGE = LocalTime.of(10, 0);
+    private static final LocalTime TIME_LATER = LocalTime.of(23, 0);
+    private static final Temperature TEMP_BELOW = TemperatureFactory.fromCelsius(0);
+    private static final Temperature TEMP_IN_RANGE = TemperatureFactory.fromCelsius(20);
+    private static final Temperature TEMP_ABOVE = TemperatureFactory.fromCelsius(30);
+
+    private TemperatureScheduler scheduler;
+
+    @BeforeEach
+    void start() {
+        this.scheduler = new TemperatureScheduler();
+    }
+
+    @Test
+    void testAddSchedule() {
+        initSchedules();
+
+        final var schedule = scheduler.getSchedules().values().stream().findAny();
+        assertEquals(TIME_RANGE, schedule.get().timeBounds());
+        assertEquals(TEMP_RANGE, schedule.get().paramBounds());
+    }
+
+    @Test
+    void testOverlapping() {
+        initSchedules();
+
+        checkUnchangedSize(TIME_RANGE);
+        checkUnchangedSize(new Bounds<>(TIME_EARLY, TIME_IN_RANGE));
+        checkUnchangedSize(new Bounds<>(TIME_IN_RANGE, TIME_LATER));
+        checkUnchangedSize(new Bounds<>(TIME_IN_RANGE, TIME_IN_RANGE));
+    }
+
+    @Test
+    void testRemoveSchedule() {
+        initSchedules();
+
+        final Optional<ScheduleId> id = scheduler.getSchedules().keySet().stream().findFirst();
+        assertTrue(id.isPresent());
+        scheduler.removeSchedule(id.get());
+        assertTrue(scheduler.getSchedules().size() == 0);
+    }
+
+    @Test
+    void testCheckSchedule() {
+        initSchedules();
+
+        assertEquals(ParameterResult.NOT_FOUND, scheduler.checkSchedule(TIME_EARLY, TEMP_IN_RANGE));
+        assertEquals(ParameterResult.NOT_FOUND, scheduler.checkSchedule(TIME_LATER, TEMP_IN_RANGE));
+
+        assertEquals(ParameterResult.BELOW_BOUNDS, scheduler.checkSchedule(TIME_IN_RANGE, TEMP_BELOW));
+        assertEquals(ParameterResult.IN_BOUNDS, scheduler.checkSchedule(TIME_IN_RANGE, TEMP_IN_RANGE));
+        assertEquals(ParameterResult.ABOVE_BOUNDS, scheduler.checkSchedule(TIME_IN_RANGE, TEMP_ABOVE));
+    }
+
+    private void initSchedules() {
+        assertTrue(scheduler.getSchedules().size() == 0);
+        scheduler.addSchedule(TIME_RANGE, TEMP_RANGE);
+        assertTrue(scheduler.getSchedules().size() == 1);
+    }
+
+    private void checkUnchangedSize(final Bounds<LocalTime> newTimeRange) {
+        final var sizeBefore = scheduler.getSchedules().size();
+        scheduler.addSchedule(newTimeRange, TEMP_RANGE);
+        assertTrue(scheduler.getSchedules().size() == sizeBefore);
+    }
+}
