8456694c2a1b473b0d3a241ab12d038ec74d9d30
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-03-29 18:44:50 +0200

    [VIEW_METER] Add simple ViewManager

---
+++ src/main/java/homer/view/javafx/electricalmeter/scenebuilder/ElectricalMeterViewManager.java
@@ -0,0 +1,73 @@
+package homer.view.javafx.electricalmeter.scenebuilder;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import homer.controller.impl.electricalmeter.ElectricalMeterImpl;
+import homer.model.outlets.Outlet;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ElectricalMeterViewManager {
+
+    @FXML
+    // Reference of consumptionLabel for the FXML loader.
+    private Label consumptionLabel;
+
+    @FXML
+    // Reference of energyLabel for the FXML loader.
+    private Label energyLabel;
+
+    @FXML
+    TableView<Outlet> outletTable;
+
+    @FXML
+    // Reference of outletStateColumn for the FXML loader.
+    private TableColumn<Outlet, Double> outletStateColumn;
+
+    @FXML
+    // Reference of outletStateColumn for the FXML loader.
+    private TableColumn<Outlet, String> outletIDColumn;
+
+    // Automatically set by FXML Loader
+    @FXML
+    private URL location;
+
+    @FXML
+    private ResourceBundle resources;
+
+    private ElectricalMeterImpl meter;
+
+    // Empty constructor
+    public ElectricalMeterViewManager() {
+    }
+
+    public void setMeter(final ElectricalMeterImpl meter) {
+        this.meter = meter;
+    }
+
+    @FXML
+    private void init() {
+    }
+
+    private static int count = 0;
+
+    @FXML
+    public void setLabels() {
+        final double consumption = meter.getGlobalConsumption();
+        final double energy = meter.getAveragePower();
+        outletTable.setItems(FXCollections.observableArrayList(meter.getOutlets()));
+        consumptionLabel.setText(String.format("Global Consumption: %.2f W", consumption));
+        energyLabel.setText(String.format("Average Energy: %.2f Wh", energy));
+        outletStateColumn.setCellValueFactory(
+                cellData -> new ReadOnlyObjectWrapper<Double>(cellData.getValue().getState().getPower().get()));
+        outletIDColumn.setCellValueFactory(
+                cellData -> new ReadOnlyObjectWrapper<String>(
+                        cellData.getValue().getClass().getSimpleName() + count++));
+    }
+
+}
