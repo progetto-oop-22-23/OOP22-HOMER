c767045343130cad2449ba0de6e79447c7cb7b0b
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-03-29 18:57:08 +0200

    [VIEW_METER] Launch testing class

--- src/main/java/homer/App.java
+++ src/main/java/homer/App.java
@@ -1,7 +1,22 @@
 package homer;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import homer.controller.impl.electricalmeter.ElectricalMeterImpl;
+import homer.model.outlets.Outlet;
+import homer.model.outlets.OutletFactory;
 import homer.view.JFXApplication;
+import homer.view.LaunchMeterView;
+import homer.view.javafx.electricalmeter.scenebuilder.ElectricalMeterViewManager;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * Main application class.
@@ -17,6 +32,9 @@ public final class App {
      * @param args Command line arguments
      */
     public static void main(final String[] args) {
+        //JUST FOR TESTING PURPOSES. Uncomment for launching ElectricalMeter's view.
+        //Application.launch(LaunchMeterView.class, args);
         Application.launch(JFXApplication.class, args);
+        
     }
 }
