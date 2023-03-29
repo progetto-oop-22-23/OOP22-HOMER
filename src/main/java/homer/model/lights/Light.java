d66dcb9c27c048a54ee3321f44178d3a5c50a1c1
Michele Ceccacci <michele.ceccacci@studio.unibo.it>
2023-03-29 23:32:03 +0200

    refactor Light with onOffState

--- src/main/java/homer/model/lights/Light.java
+++ src/main/java/homer/model/lights/Light.java
@@ -7,6 +7,7 @@ import homer.api.PoweredDevice;
 import homer.api.PoweredDeviceInfo;
 import homer.api.PoweredDeviceInfoImpl;
 import homer.api.ToggleableDevice;
+import homer.api.state.OnOffState;
 import homer.common.limit.Limit;
 import homer.common.time.DurationConverter;
 import homer.core.DiscreteObject;
@@ -18,9 +19,9 @@ import homer.model.outlets.OutletFactory;
  * 
  * @author Alessandro Monticelli
  */
-public final class Light implements ToggleableDevice<Boolean>, PoweredDevice, DiscreteObject {
+public final class Light implements ToggleableDevice<OnOffState>, PoweredDevice, DiscreteObject {
 
-    private Boolean state;
+    private OnOffState state;
     private final PoweredDeviceInfo power;
     private double instantConsumption;
 
@@ -31,7 +32,7 @@ public final class Light implements ToggleableDevice<Boolean>, PoweredDevice, Di
      * @param power See {@link homer.api.PoweredDeviceInfo}.
      */
     public Light(final Boolean state, final PoweredDeviceInfo power) {
-        this.state = Objects.requireNonNull(state);
+        this.state = new OnOffState(Objects.requireNonNull(state));
         this.power = new PoweredDeviceInfoImpl(power.getMaxConsumption(), power.getOutlet());
         this.instantConsumption = 0.0;
     }
@@ -42,25 +43,25 @@ public final class Light implements ToggleableDevice<Boolean>, PoweredDevice, Di
      * @param state On/Off.
      */
     public Light(final Boolean state) {
-        this.state = Objects.requireNonNull(state);
+        this.state = new OnOffState(Objects.requireNonNull(state));
         this.power = new PoweredDeviceInfoImpl(10.0,
                 OutletFactory.cOutlet(0));
         this.instantConsumption = 0.0;
     }
 
     @Override
-    public Boolean getState() {
-        return this.state;
+    public OnOffState getState() {
+        return new OnOffState(this.isToggled());
     }
 
     @Override
     public boolean isToggled() {
-        return this.getState();
+        return this.state.isOn();
     }
 
     @Override
     public void toggle() {
-        this.state ^= true;
+        this.state = new OnOffState(!this.getState().isOn());
     }
 
     @Override
