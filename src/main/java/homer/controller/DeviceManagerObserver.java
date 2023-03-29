dcefd87a7cee4416dc59294a8a7edf2d3ed48d6d
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-03-29 13:49:22 +0200

    [controller] refactor DeviceManager by separating logic and view methods

---
+++ src/main/java/homer/controller/DeviceManagerObserver.java
@@ -0,0 +1,49 @@
+package homer.controller;
+
+import java.util.Set;
+
+import homer.api.DeviceId;
+import homer.api.DeviceState;
+import homer.controller.command.createdevicecommand.CreateDeviceCommand;
+
+/**
+ * Interface of the device manager to be called from the view.
+ */
+public interface DeviceManagerObserver {
+
+    /**
+     * Removes a device.
+     * 
+     * @param deviceId the device to be removed.
+     */
+    void removeDevice(DeviceId deviceId);
+
+    /**
+     * Removes all Devices.
+     */
+    void removeAllDevices();
+
+    /**
+     * Returns the set of {@link CreateDeviceCommand} commands that can be displayed
+     * (and sent back to the controller) to the view.
+     * 
+     * @return Set of valid commands.
+     */
+    public Set<CreateDeviceCommand> getValidCreateDeviceCommands();
+
+    /**
+     * Receives an update command from the view.
+     * 
+     * @param deviceId the id of the device to update.
+     * @param state    the updated state.
+     */
+    public void updateDeviceState(DeviceId deviceId, DeviceState state);
+
+    /**
+     * Toggles a device whose state is toggleable.
+     * 
+     * @param deviceId the device to be toggled.
+     */
+    void toggleDevice(DeviceId deviceId);
+
+}
