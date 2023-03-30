e951b27af626b03a848570285ecf66bd1693e637
Michele Ceccacci <michele.ceccacci@studio.unibo.it>
2023-03-30 09:52:37 +0200

    progress on logger

--- src/main/java/homer/view/logger/LoggerImpl.java
+++ src/main/java/homer/view/logger/LoggerImpl.java
@@ -9,9 +9,14 @@ import java.util.Objects;
 import homer.api.DeviceId;
 import homer.api.DeviceState;
 import homer.api.state.ActuatedDeviceState;
+import homer.api.state.OnOffState;
+import homer.api.state.ThermometerState;
 import homer.controller.Controller;
+import homer.model.outlets.Outlet;
+import homer.model.outlets.OutletState;
 import homer.model.temperaturechangers.TemperatureChangerState;
 import homer.model.temperaturechangers.TemperatureChangerType;
+import homer.model.thermometer.Thermometer;
 
 /**
  * LoggerImpl
@@ -32,29 +37,50 @@ public final class LoggerImpl implements Logger {
     @Override
     public void updateDeviceState(final DeviceId deviceId, final DeviceState deviceState) {
         if (!stringReps.containsKey(deviceId)) {
-            log("ADD DEVICE:" + deviceId.toString() + ":");
+            String stringRep = "";
             if (deviceState instanceof TemperatureChangerState) {
-                final var state = (TemperatureChangerState)  deviceState;
-                final String typeStringRep;
+                final var state = (TemperatureChangerState) deviceState;
                 if (state.getType().get().equals(TemperatureChangerType.AIRCONDITIONING)) {
-                    typeStringRep = "Air conditioning";
+                    stringRep = "Air conditioning";
+                } else {
+                    stringRep = "Heating";
                 }
-                else {
-                    typeStringRep = "Heating";
+            } else if (deviceState instanceof ActuatedDeviceState) {
+                final var state = (ActuatedDeviceState) deviceState;
+                stringRep = "Actuated device";
+                if (state.getPositionBounds().isPresent()) {
+                    stringRep += state.getPositionBounds().get().toString();
                 }
-                stringReps.put(deviceId, typeStringRep);
-                log(typeStringRep);
+            } else if (deviceState instanceof OnOffState) {
+
+            } else if (deviceState instanceof OutletState) {
+            } else if (deviceState instanceof ThermometerState) {
+                stringRep = "Thermometer";
             }
-            if (deviceState instanceof ActuatedDeviceState) {
-            }
+            stringReps.put(deviceId, stringRep);
+            log("ADD DEVICE:" + deviceId.toString() + ":" + stringRep);
+
         } else {
-            log("UPDATE DEVICE:");
+            log("UPDATE DEVICE:" + deviceId.toString() + ":" + stringReps.get(deviceId));
+        }
+        if (deviceState instanceof TemperatureChangerState) {
+            final var state = (TemperatureChangerState) deviceState;
+            log(state.getCurrentIntensity().get().toString());
+        } else if (deviceState instanceof ActuatedDeviceState) {
+            final var state = (ActuatedDeviceState) deviceState;
+            log(Integer.toString(state.getPosition()));
+        } else if (deviceState instanceof OnOffState) {
+            log("TURNED ON:" + ((OnOffState) deviceState).isOn());
+        } else if (deviceState instanceof OutletState) {
+            log("POWER:" + ((OutletState) deviceState).getPower().get());
+        } else if (deviceState instanceof ThermometerState) {
+            log("TEMPERATURE:" + ((ThermometerState) deviceState).getTemperature().getCelsius() + "C");
         }
     }
 
     @Override
     public void removeDevice(final DeviceId deviceId) {
-        log("REMOVE DEVICE:" + deviceId.toString() + ":" + stringReps.get(deviceId));
+        log("REMOVE DEVICE:" + deviceId.toString() + ":" + stringReps.get(deviceId) + "\n");
         stringReps.remove(deviceId);
     }
 
