93e128c066adb175c2d963fb3025318080dfe390
Michele Ceccacci <michele.ceccacci@studio.unibo.it>
2023-03-30 15:28:19 +0200

    minor fixes to JFXDeviceViewer.java

--- src/main/java/homer/view/javafx/JFXDeviceViewer.java
+++ src/main/java/homer/view/javafx/JFXDeviceViewer.java
@@ -9,13 +9,13 @@ import homer.api.DeviceView;
 import homer.controller.Controller;
 import homer.model.temperaturechangers.TemperatureChangerState;
 import homer.view.DeviceViewer;
+import javafx.scene.Node;
 import javafx.scene.layout.Pane;
 
 public class JFXDeviceViewer extends Pane implements DeviceViewer {
 
     private Controller controller;
     private final Map<DeviceId, DeviceView> deviceMap = new LinkedHashMap<>();
-
 
     public JFXDeviceViewer(Controller controller) {
         this.getChildren().add(new AddDevicesView(controller));
@@ -26,10 +26,10 @@ public class JFXDeviceViewer extends Pane implements DeviceViewer {
         if (this.deviceMap.containsKey(deviceId)) {
             final var deviceView = this.deviceMap.get(deviceId);
             deviceView.setState(deviceState);
-        }
-        else {
+        } else {
             if (deviceState instanceof TemperatureChangerState) {
-                var deviceView = new TemperatureChangerView(deviceId, (TemperatureChangerState) deviceState, controller);
+                var deviceView = new TemperatureChangerView(deviceId, (TemperatureChangerState) deviceState,
+                        controller);
                 this.deviceMap.put(deviceId, deviceView);
                 this.getChildren().add(deviceView);
             }
@@ -40,7 +40,9 @@ public class JFXDeviceViewer extends Pane implements DeviceViewer {
     public void removeDevice(final DeviceId deviceId) {
         if (deviceMap.containsKey(deviceId)) {
             final var target = deviceMap.get(deviceId);
-            this.getChildren().remove(this.deviceMap.get(deviceId));
+            // since all javaFX components (and derived classes) we are using extend Node,
+            // this is fine
+            this.getChildren().remove((Node) target);
             deviceMap.remove(deviceId);
         }
     }
@@ -49,5 +51,5 @@ public class JFXDeviceViewer extends Pane implements DeviceViewer {
     public void start(Controller controller) {
         this.controller = controller;
     }
-    
+
 }
