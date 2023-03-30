493e667bfb0f9d7868f6e5b25275a9360ee3c55e
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-03-30 21:30:17 +0200

    [scheduler] refactor scheduler view with RangeSlider

--- src/main/java/homer/view/scheduler/AddTemperatureScheduleViewFx.java
+++ src/main/java/homer/view/scheduler/AddTemperatureScheduleViewFx.java
@@ -2,13 +2,14 @@ package homer.view.scheduler;
 
 import java.time.LocalTime;
 import java.util.function.BiConsumer;
+import java.util.stream.Stream;
+
+import org.controlsfx.control.RangeSlider;
 
 import homer.common.bounds.Bounds;
 import homer.common.temperature.Temperature;
 import homer.common.temperature.TemperatureFactory;
 import javafx.scene.control.Button;
-import javafx.scene.control.Slider;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 /**
@@ -20,59 +21,34 @@ public final class AddTemperatureScheduleViewFx extends VBox {
     private static final double TIME_MAX = 24;
     private static final double TEMP_MIN = 15;
     private static final double TEMP_MAX = 30;
-    private final Slider timeFrom = new Slider(TIME_MIN, TIME_MAX, TIME_MIN);
-    private final Slider timeTo = new Slider(TIME_MIN, TIME_MAX, TIME_MIN);
-    private final Slider paramFrom = new Slider(TEMP_MIN, TEMP_MAX, TEMP_MIN);
-    private final Slider paramTo = new Slider(TEMP_MIN, TEMP_MAX, TEMP_MIN);
+    private static final double MAJOR_TICK_UNIT = 1.0d;
+    private static final int MINOR_TICK_UNIT = 0;
+    private final RangeSlider timeRange = new RangeSlider(TIME_MIN, TIME_MAX, TIME_MIN, TIME_MAX);
+    private final RangeSlider tempRange = new RangeSlider(TEMP_MIN, TEMP_MAX, TEMP_MIN, TEMP_MAX);
     private final Button addBtn = new Button("Add");
-    private final HBox timeBox = new HBox();
-    private final HBox paramBox = new HBox();
 
     public AddTemperatureScheduleViewFx(final BiConsumer<Bounds<LocalTime>, Bounds<Temperature>> addAction) {
-
-        timeFrom.setShowTickLabels(true);
-        timeTo.setShowTickLabels(true);
-        paramFrom.setShowTickLabels(true);
-        paramTo.setShowTickLabels(true);
-
-        // timeFrom.setShowTickMarks(true);
-        // timeTo.setShowTickMarks(true);
-        // paramFrom.setShowTickMarks(true);
-        // paramTo.setShowTickMarks(true);
-
-        timeFrom.setBlockIncrement(1.0d);
-        timeTo.setBlockIncrement(1.0d);
-        paramFrom.setBlockIncrement(0.1d);
-        paramTo.setBlockIncrement(0.1d);
-
-        timeFrom.setMajorTickUnit(1.0d);
-        timeTo.setMajorTickUnit(1.0d);
-        paramFrom.setMajorTickUnit(0.1d);
-        paramTo.setMajorTickUnit(0.1d);
-
-        timeFrom.setSnapToTicks(true);
-        timeTo.setSnapToTicks(true);
-        paramFrom.setSnapToTicks(true);
-        paramTo.setSnapToTicks(true);
+        Stream.of(timeRange, tempRange).forEach(rs -> {
+            rs.setShowTickLabels(true);
+            rs.setShowTickMarks(true);
+            rs.setBlockIncrement(MAJOR_TICK_UNIT);
+            rs.setMajorTickUnit(MAJOR_TICK_UNIT);
+            rs.setMinorTickCount(MINOR_TICK_UNIT);
+            rs.setSnapToTicks(true);
+        });
 
         this.addBtn.setOnAction(ev -> {
-            final var tempMin = TemperatureFactory.fromCelsius(paramFrom.getValue());
-            final var tempMax = TemperatureFactory.fromCelsius(paramTo.getValue());
+            final var tempMin = TemperatureFactory.fromCelsius(tempRange.getLowValue());
+            final var tempMax = TemperatureFactory.fromCelsius(tempRange.getHighValue());
             addAction.accept(
-                    new Bounds<>(LocalTime.of((int) timeFrom.getValue(), 0), LocalTime.of((int) timeTo.getValue(), 0)),
+                    new Bounds<>(LocalTime.of((int) timeRange.getLowValue(), 0),
+                            LocalTime.of((int) timeRange.getHighValue() - 1, 59)), // TODO fix logic bug away from view
                     new Bounds<>(tempMin, tempMax));
         });
-        timeBox.getChildren().addAll(timeFrom, timeTo);
-        paramBox.getChildren().addAll(paramFrom, paramTo);
-        this.getChildren().addAll(timeBox, paramBox, addBtn);
+        this.getChildren().addAll(timeRange, tempRange, addBtn);
 
-        timeFrom.autosize();
-        timeTo.autosize();
-        paramFrom.autosize();
-        paramTo.autosize();
-
-        timeBox.autosize();
-        paramBox.autosize();
+        timeRange.autosize();
+        tempRange.autosize();
 
         this.autosize();
     }
