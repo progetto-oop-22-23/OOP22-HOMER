2785df529a06e519b54756faa9caace3c5c36744
Michele Ceccacci <michele.ceccacci@studio.unibo.it>
2023-04-01 14:42:50 +0200

    [view] introduce air quality view

---
+++ src/main/java/homer/view/javafx/deviceview/AirqualityView.java
@@ -0,0 +1,28 @@
+package homer.view.javafx.deviceview;
+
+import homer.api.DeviceState;
+import homer.api.DeviceView;
+import homer.model.airquality.AirQualityState;
+import homer.view.javafx.JFXDeviceView;
+import javafx.application.Platform;
+import javafx.scene.control.Label;
+
+public class AirqualityView extends JFXDeviceView {
+    private final Label pm25 =  new Label();
+    private final Label pm10 =  new Label();
+    private final Label toxicGasPercentage =  new Label();
+    private final Label co2 =  new Label();
+
+    @Override
+    public void setState(DeviceState state) {
+        if (state instanceof AirQualityState airQualityState) {
+            Platform.runLater(()-> {
+                this.pm10.setText("PM10: " + airQualityState.getPM10());
+                this.pm25.setText("PM25: " + airQualityState.getPM25());
+                this.co2.setText("CO2: " + airQualityState.getCO2());
+                this.toxicGasPercentage.setText("CO2: " + airQualityState.getToxicGasPercentage());
+            });
+        }
+    }
+    
+}
