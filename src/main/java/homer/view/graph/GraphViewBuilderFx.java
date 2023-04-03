3a388c5f8d78b6e4537da17e7eb344c673bb9465
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-04-03 21:08:21 +0200

    [graph] add utility builder class for the graph tab

---
+++ src/main/java/homer/view/graph/GraphViewBuilderFx.java
@@ -0,0 +1,45 @@
+package homer.view.graph;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javafx.scene.Node;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TabPane.TabClosingPolicy;
+import javafx.scene.control.TabPane.TabDragPolicy;
+
+/**
+ * Utility class to build the graph tabs.
+ */
+public final class GraphViewBuilderFx {
+
+    private final Map<String, Node> graphs = new HashMap<>();
+
+    /**
+     * Add a new tab with the given graph.
+     * 
+     * @param tabName the tab title name.
+     * @param graph   the graph node.
+     * @return the builder.
+     */
+    public GraphViewBuilderFx addGraph(final String tabName, final AbstractGraphViewFx<?> graph) {
+        this.graphs.put(tabName, graph);
+        return this;
+    }
+
+    /**
+     * Builds the tabpane with the graph tabs.
+     * 
+     * @return the tabpane with the graph tabs.
+     */
+    public TabPane build() {
+        final TabPane tabPane = new TabPane();
+        tabPane.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
+        tabPane.setTabDragPolicy(TabDragPolicy.REORDER);
+        graphs.forEach((name, node) -> tabPane.getTabs().add(new Tab(name, node)));
+        tabPane.autosize();
+        return tabPane;
+    }
+
+}
