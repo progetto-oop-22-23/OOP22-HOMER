794be88f22a8a881af15f83ff124b6635dd17538
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-04-04 18:59:17 +0200

    [graph] add initial AirQualityGraphFx

---
+++ src/main/java/homer/view/graph/AirQualityGraphFx.java
@@ -0,0 +1,17 @@
+package homer.view.graph;
+
+import java.util.Set;
+
+import homer.common.history.HistoryData;
+import homer.model.airquality.AirQualityState;
+import javafx.scene.layout.VBox;
+
+public final class AirQualityGraphFx extends VBox implements GraphView<AirQualityState> {
+
+    @Override
+    public void updateGraph(final Set<HistoryData<AirQualityState>> historyData) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateGraph'");
+    }
+
+}
