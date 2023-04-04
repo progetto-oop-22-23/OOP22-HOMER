029851c5a60f31597d40bb5c527bc61c599c5b09
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-04-04 11:06:53 +0200

    [graph] refactor AbstractLogger
    Add the log field, and make the supplier return an optional

--- src/main/java/homer/controller/history/AbstractLogger.java
+++ src/main/java/homer/controller/history/AbstractLogger.java
@@ -1,33 +1,34 @@
 package homer.controller.history;
 
 import java.time.Duration;
+import java.util.Optional;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import homer.common.history.HistoryData;
 import homer.model.history.HistoricalDataLogger;
+import homer.model.history.HistoricalDataLoggerImpl;
 import homer.view.graph.GraphView;
 
 public abstract class AbstractLogger<T> implements HistoricalDataController<T> {
 
     private static final long LIMIT_ENTRIES = 30;
     private static final Duration LOG_INTERVAL = Duration.ofHours(1);
+    private final HistoricalDataLogger<T> log = new HistoricalDataLoggerImpl<>();
     private final GraphView<T> view;
-    private final Supplier<HistoryData<T>> dataSupplier;
+    private final Supplier<Optional<HistoryData<T>>> dataSupplier;
     private Duration accumulatedTime = Duration.ofNanos(0);
 
-    protected AbstractLogger(final Supplier<HistoryData<T>> dataSupplier, final GraphView<T> view) {
+    protected AbstractLogger(final Supplier<Optional<HistoryData<T>>> dataSupplier, final GraphView<T> view) {
         this.dataSupplier = dataSupplier;
         this.view = view;
     }
 
-    abstract protected HistoricalDataLogger<T> getLog();
-
     @Override
-    public void updateTick(final Duration deltaTime) {
+    public final void updateTick(final Duration deltaTime) {
         if (this.accumulatedTime.compareTo(LOG_INTERVAL) >= 0) {
-            getLog().logData(dataSupplier.get());
-            final var historyData = getLog().getHistory();
+            dataSupplier.get().ifPresent(hd -> this.log.logData(hd));
+            final var historyData = this.log.getHistory();
             this.view.updateGraph(historyData.stream()
                     .sorted()
                     .skip(historyData.size() < LIMIT_ENTRIES ? 0 : historyData.size() - LIMIT_ENTRIES)
