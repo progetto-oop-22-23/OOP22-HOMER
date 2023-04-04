e2ca5669c4a882354dbabbe20302cf55deb4700b
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-04-04 22:18:46 +0200

    [graph] update javadoc of ConsumptionGraphFx

--- src/main/java/homer/view/graph/ConsumptionGraphFx.java
+++ src/main/java/homer/view/graph/ConsumptionGraphFx.java
@@ -1,9 +1,16 @@
 package homer.view.graph;
 
+/**
+ * Implementation of {@link TemplateGraphViewFx} for the view of energy global
+ * consumption.
+ */
 public final class ConsumptionGraphFx extends TemplateGraphViewFx<Double> {
 
     private static final String TITLE = "Global energy consumption (Wh)";
 
+    /**
+     * Creates a new {@link ConsumptionGraphFx}.
+     */
     public ConsumptionGraphFx() {
         super(TITLE, d -> d);
     }
