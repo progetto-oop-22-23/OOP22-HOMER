10a1eb1a6483aaa09be8883f7ad5c521951385d4
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-04-05 17:11:57 +0200

    sim view qol improvements
    Changed rate and datetime format.

--- src/main/java/homer/view/sim/SimManagerViewFxImpl.java
+++ src/main/java/homer/view/sim/SimManagerViewFxImpl.java
@@ -19,9 +19,9 @@ public final class SimManagerViewFxImpl extends VBox implements SimManagerView {
 
     private static final String RESUME = "Resume";
     private static final String PAUSE = "Pause";
-    private static final long ONE_X = 1L;
-    private static final long FIFTY_X = 50L;
-    private static final long FIVE_THOUSAND_X = 5000L;
+    private static final long ONE = 1L;
+    private static final long TWO_FIVE_HUNDRED = 2500L;
+    private static final long FIVE_THOUSAND = 5000L;
     private static final String RATE_TEXT = "Time rate: ";
     private final HBox buttonsBox = new HBox();
     private final HBox clockBox = new HBox();
@@ -36,17 +36,17 @@ public final class SimManagerViewFxImpl extends VBox implements SimManagerView {
     public SimManagerViewFxImpl() {
         final Button resume = new Button(RESUME);
         final Button pause = new Button(PAUSE);
-        final Button oneX = new Button(ONE_X + "x");
-        final Button fiftyX = new Button(FIFTY_X + "x");
-        final Button fiveThousandX = new Button(FIVE_THOUSAND_X + "x");
+        final Button normalRateBtn = new Button(ONE + "x");
+        final Button mediumRateBtn = new Button(TWO_FIVE_HUNDRED + "x");
+        final Button fastRateBtn = new Button(FIVE_THOUSAND + "x");
 
         resume.setOnAction(event -> this.simManager.resume());
         pause.setOnAction(event -> this.simManager.pause());
-        oneX.setOnAction(event -> this.simManager.setTimeRate(ONE_X));
-        fiftyX.setOnAction(event -> this.simManager.setTimeRate(FIFTY_X));
-        fiveThousandX.setOnAction(event -> this.simManager.setTimeRate(FIVE_THOUSAND_X));
+        normalRateBtn.setOnAction(event -> this.simManager.setTimeRate(ONE));
+        mediumRateBtn.setOnAction(event -> this.simManager.setTimeRate(TWO_FIVE_HUNDRED));
+        fastRateBtn.setOnAction(event -> this.simManager.setTimeRate(FIVE_THOUSAND));
 
-        buttonsBox.getChildren().addAll(resume, pause, oneX, fiftyX, fiveThousandX);
+        buttonsBox.getChildren().addAll(resume, pause, normalRateBtn, mediumRateBtn, fastRateBtn);
         clockBox.getChildren().addAll(datetime);
         rateBox.getChildren().addAll(new Text(RATE_TEXT), timerate);
         this.getChildren().addAll(buttonsBox, clockBox, rateBox);
@@ -59,12 +59,13 @@ public final class SimManagerViewFxImpl extends VBox implements SimManagerView {
 
     @Override
     public void setDateTime(final LocalDateTime simTime) {
-        Platform.runLater(() -> this.datetime.setText(simTime.format(DateTimeFormatter.ISO_DATE_TIME)));
+        Platform.runLater(
+                () -> this.datetime.setText(simTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))));
     }
 
     @Override
     public void setTimeRate(final long timeRate) {
-        Platform.runLater(() -> this.timerate.setText(Long.toString(timeRate)));
+        Platform.runLater(() -> this.timerate.setText(Long.toString(timeRate) + "x"));
     }
 
     @Override
