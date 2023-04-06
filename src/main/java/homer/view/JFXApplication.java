f86643e62e25ed0023124fa0da28fb1b72066920
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-04-06 16:16:55 +0200

    remove comment

--- src/main/java/homer/view/JFXApplication.java
+++ src/main/java/homer/view/JFXApplication.java
@@ -117,8 +117,7 @@ public class JFXApplication extends Application {
         final ScrollPane dashboardScrollPane = new ScrollPane(dashboard);
         dashboardScrollPane.setFitToHeight(true);
         dashboardScrollPane.setFitToWidth(true);
-        // TODO separate the add devices from the device list, so that only the list is
-        // scrollable
+
         final Tab devicesView = new Tab("DEVICES", dashboardScrollPane);
         final Tab schedulerTab = new Tab("SCHEDULER", tempSchedulerView);
         final Tab graphView = new Tab("GRAPHS",
